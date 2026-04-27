ae54722e9578d3309e1bedb31ef3327d007d4c27
JohnAAbbott <124266874+JohnAAbbott@users.noreply.github.com>
2026-04-27 21:58:56 +0200

    Update src/Matrix.jl
    Co-authored-by: Lars Göttgens <lars.goettgens@gmail.com>

--- src/Matrix.jl
+++ src/Matrix.jl
@@ -6631,7 +6631,7 @@ end
 @doc raw"""
     multiply_row(a::MatrixElem{T}, s::RingElement, i::Int, cols = 1:ncols(a)) where T <: RingElement
 
-Create a copy of $a$ and multiply  the $i$-th row of $a$ with $s$.
+Create a copy of $a$ and multiply the $i$-th row of $a$ with $s$.
 
 By default, the transformation is applied to all columns of $a$. This can be
 changed using the optional `cols` argument.
